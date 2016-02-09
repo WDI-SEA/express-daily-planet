d7bef3cd4f18979a8ea40a65195b375b3a13b4bc
steampunklink <nethhorn@gmail.com>
2016-02-08 16:42:13 -0800

    root route renders home page

--- index.js
+++ index.js
@@ -2,11 +2,15 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
 
+var articles = [
+	{title: 'Article Title', body: 'This is where the body of the article goes.'}
+];
+
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: false}));
 
 app.get("/",function(req,res){
-  res.send('HELLO TACO!!!');
+  res.render('index.ejs');
 });
 
 app.listen(3000);
