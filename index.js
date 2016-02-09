fa9d65785119fce9621dda5c5778820ea7c6ea05
Scott <scott.heron@outlook.com>
2016-02-08 18:34:43 -0800

    mid homework

---
+++ index.js
@@ -0,0 +1,47 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+
+var articles = [
+  {title: 'Article 1', body: 'this is the first article body'},
+  {title: 'Article 2', body: 'this is the second article body'},
+  {title: 'Article 3', body: 'this is the third article body'},
+  {title: 'Article 4', body: 'this is the fourth article body'},
+  {title: 'Article 5', body: 'this is the fith article body'},
+  {title: 'Article 6', body: 'this is the sixth article body'}
+];
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.get('/',function(req,res){
+  res.render('index.ejs');
+});
+
+app.get('/articles', function(req, res){
+	res.render('articles/index.ejs', {articles: articles});
+});
+
+app.get('/articles/new', function(req, res){
+	res.render('articles/new.ejs');
+});
+
+app.get('/about', function(req, res){
+	res.render('site/about.ejs');
+});
+
+app.get('/contact', function(req, res){
+	res.render('site/contact.ejs')
+});
+
+app.post('/articles', function(req, res){
+	articles.push(req.body);
+	res.redirect('/articles');
+});
+
+app.get('/articles/:idx', function(req, res) {
+  var articleIdx = parseInt(req.params.idx);
+  res.render('show.ejs', {article: articles[articleIdx]});
+});
+
+app.listen(3000);
