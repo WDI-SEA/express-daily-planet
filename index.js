44b7ea7104fb5fe4073605ff05000a9f85599938
navyayvan <navyayvan@gmail.com>
2016-02-08 17:15:32 -0800

    routes are functional

--- index.js
+++ index.js
@@ -4,26 +4,44 @@ var app = express();
 
 //array of articles
 var articles = [
-  {title: 'Article title', body: 'this is the first article body'}
+  {title: 'Cat eats loaf of bread', body: 'Its incredible'},
+  {title: 'Man survives volcano', body: 'Invented sweet clothes'},
+  {title: 'Bird saves baby from fire', body: 'Drops into arms of a policeman' }
 ];
 
 
 // set view engine to index.ejs
 app.set('view engine', 'ejs');
 
+//use body parser
+app.use(bodyParser.urlencoded({extended: false}));
+
 //serves the homepage of the site 
-app.get("/",function(req,res){
-  res.render('views/index.ejs');
+app.get("/", function(req,res) {
+  res.render('index.ejs');
 });
 
 //displays a list of all articles
 app.get("/articles", function(req,res) {
-	res.render('views/articles/index.ejs');
-})
+	res.render('articles/index.ejs', {articles: articles});
+});
 
 //displays a form that users use to create a new article
-app.get("articles/new", function(req,res) {
-	res.render('views/articles/new.ejs');
+app.get("/articles/new", function(req,res) {
+	res.render('articles/new.ejs');
+});
+
+//creates a new article (adds to article array)
+app.post('/articles', function (req,res) {
+	articles.push(req.body);
+	console.log(articles);
+	res.redirect('/articles');
+
+})
+
+//find, by id, an article in the array, and displays the article.
+app.get('/articles/:id', function(req,res) {
+	res.render('articles/show.ejs');
 })
 
 // listen
