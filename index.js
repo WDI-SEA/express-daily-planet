79e4b032b87477e0fcdf2a2c13365d7c46bf6c7d
brytree <brycebortree@gmail.com>
2016-04-11 15:22:23 -0700

    up through routes

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var app = express();
 
+var articles = [
+{title: 'Lex Luthor strikes again!', body: 'Why is he after us?'},
+{title: 'Lex Luthor strikes!', body: 'Is he partially lightning?'},
+{title: 'Batman V Superman', body: 'Actually a super boring fight'},
+{title: 'Staff reporters wed!', body: 'Lois Lane hospitalized with broken pelvis on honeymoon'},
+{title: 'Spiderman?', body: 'At least we are not New York right?'},
+{title: 'Kryptonite Taste Test', body: 'Staff reporter Clark Kent gives them two thumbs down.'}];
+
 app.get("/",function(req,res){
   res.render('index.ejs');
-  res.send('All is well');
+  console.log('All is well');
+});
+
+app.get("/articles", function(req, res) {
+
 });
 
 app.listen(3000);
