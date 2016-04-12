42dd5f221e5507b963a508f51b74a63b7d175b74
montehoover <monte.b.hoover@gmail.com>
2016-04-11 17:20:13 -0700

    fully functional, no formatting yet.

---
+++ index.js
@@ -0,0 +1,53 @@
+///////////
+// Dependencies
+///////////
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+
+var articles = [
+  {title: 'Daily Planet Opens in Tacoma', body: 'The world\'s formost news service opens offices in the South Sound\'s hippest city.'},
+];
+
+//////////
+// Middleware
+//////////
+app.use('/site', express.static(__dirname + '/views/site'));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: false}));
+
+
+//////////
+// Routs (URLs)
+//////////
+app.get('/', function(req, res) {
+  res.render('index.ejs', {articles: articles});
+});
+
+app.get('/articles', function(req, res) {
+  res.render('articles/index.ejs', {articles: articles});
+});
+
+// page that lets you submit a new article
+app.get('/articles/new', function(req, res) {
+  res.render('articles/new.ejs');
+});
+
+// Process req.body and display some acknowledgement or new page
+app.post('/articles/new', function(req, res) {
+  console.log(req.body);
+  articles.push(req.body);
+  res.redirect('/articles');
+});
+
+// View a single article by id
+app.get('/articles/:i', function(req, res) {
+  res.render('articles/single-article.ejs', {articles: articles, i: parseInt(req.params.i)});
+});
+
+
+
+//////////
+// Start Server
+//////////
+app.listen(3000)
