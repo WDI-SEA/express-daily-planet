7f1a2c65b93051df563b00eadba483624e2f6770
emilieg <emilie.gerber@hotmail.com>
2016-04-12 10:31:05 -0700

    fixed search functionality and added picture to home page

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 var express = require('express'); 
 var bodyParser = require('body-parser');
 var app = express(); 
+var url = require('url'); //diff library
 
 var articles = [
-  {title: 'BOOM', body: 'Boom shackalack hizzle my shizz platea the bizzle. Donec dapibizzle. Fo shizzle mah nizzle fo rizzle, mah home g-dizzle tellus urna, pretium boofron, we gonna chung ghetto, eleifend , nunc. Brizzle suscipizzle. Integizzle sempizzle dang sizzle purus.'},
-  {title: 'FUNKY', body: 'Crackalackin vitae turpis funky fresh you son of a bizzle bibendizzle stuff. Fo shizzle pulvinizzle away velizzle. Aliquam erat volutpizzle.'},
-  {title: 'MAMMASAY', body: 'Mammasay mammasa mamma oo sa for sure. Donizzle pizzle, est izzle mah nizzle aliquet, stuff sizzle ultricizzle sheezy, gangster ullamcorper urna the bizzle fizzle check it out.'},
-  {title: 'SAPIEN', body: 'Maecenas sapien pot, iaculizzle shizznit, cool sizzle, egestizzle tellivizzle, erizzle.'}
+  {id: 0, title: 'BOOM', body: 'Boom shackalack hizzle my shizz platea the bizzle. Donec dapibizzle. Fo shizzle mah nizzle fo rizzle, mah home g-dizzle tellus urna, pretium boofron, we gonna chung ghetto, eleifend , nunc. Brizzle suscipizzle. Integizzle sempizzle dang sizzle purus.'},
+  {id: 1, title: 'FUNKY', body: 'Crackalackin vitae turpis funky fresh you son of a bizzle bibendizzle stuff. Fo shizzle pulvinizzle away velizzle. Aliquam erat volutpizzle.'},
+  {id: 2,title: 'MAMMASAY', body: 'Mammasay mammasa mamma oo sa for sure. Donizzle pizzle, est izzle mah nizzle aliquet, stuff sizzle ultricizzle sheezy, gangster ullamcorper urna the bizzle fizzle check it out.'},
+  {id: 3,title: 'SAPIEN', body: 'Maecenas sapien pot, iaculizzle shizznit, cool sizzle, egestizzle tellivizzle, erizzle.'}
 ];
 
 
@@ -21,7 +22,7 @@ app.get('/',function(req,res){
 
 app.get('/articles', function(req, res){ 
   if(!req.query.search) {
-    res.render('articles/index', {myArticles: articles})
+    res.render('articles/index.ejs', {myArticles: articles})
   }else {
     var query = req.query.search; // relates to name="search" in html form
     var results = [];
@@ -30,7 +31,7 @@ app.get('/articles', function(req, res){
         results.push(articles[i]);
       }
     }
-    res.render('articles/index.ejs', {myArticles: results});
+    res.render('articles/index', {myArticles: results});
   }
 })
 
@@ -40,9 +41,9 @@ app.post('/articles', function(req,res){
   res.redirect('/articles') // to refresh the page - url
 });
 
-app.get('/articles/new', function(req,res){
-  res.render('articles/new');
-});
+// app.get('/articles/new', function(req,res){
+//   res.render('articles/new');
+// });
 
 app.get('/about', function(req,res){
   res.render('about');
@@ -52,11 +53,40 @@ app.get('/contact', function(req,res){
   res.render('contact');
 })
 
+//diff way to search using url lib
+// app.get('/articles', function(req, res){
+//   var url_parts = url.parse(req.url, true);
+//   var query = url_parts.query;
+//   var searchTerm = query.q;
+//   var results = [];
+
+//   if(!searchTerm) {
+//     res.render('articles/index', {myArticles: articles});
+//   } else {
+//     articles.forEach(function(article) {
+//     var isTitleMatch = articles.title.indexOf(searchTerm != -1;
+//     var isContentMatch = articles.body.indexOf(searchTerm != -1;
+//       if (isTitleMatch || isContentMatch) 
+//       results.push(article);
+//   } 
+// });  
+//     res.render("articles", {articles: results});
+//  } 
+// });
+
+
+
+
+
+
+
+
+
 
 
 app.get('/articles/:index', function(req,res){
   var articleIndex = parseInt(req.params.index);
-
+  console.log(articles, articleIndex);
   res.render('articles/show', {myArticles: articles[articleIndex]});
 });
 
