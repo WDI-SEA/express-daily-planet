d1c2c40b4bb2fedff2b5ee612b0ed15da4a7d306
StrongSad <jacobkirkbarrus@gmail.com>
2016-04-12 10:22:17 -0700

    did some od the stuff in class to make it better

--- index.js
+++ index.js
@@ -1,22 +1,43 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var url = require('url');
 //middle wear
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/views'));
 app.use(bodyParser.urlencoded({extended: false}) );
 
 var articles = [
-	{title : 'title', body: 'body'}	
+	{title : 'bob', body: 'this is the body of bob, he is very bodily'},
+	{title : 'hello bob', body: 'hello body of bob, I am saying hello to you this fine marrow'},
+	{title : 'bye bob', body: 'goodbye body of bob, I am leaving you now bob'},
+	{title : 'left', body: 'I am left handed... not left handed people are fools'}	
 ];
 
 
 app.get("/",function(req,res){
-  res.render('index', {articles: articles});
+  	res.render('index', {articles: articles});
 });
 
 app.get("/articles",function(req,res){
-  res.render('articles/index', {articles: articles});
+ 	var url_parts = url.parse(req.url, true);
+	var query = url_parts.query;
+	var results = [];
+	var searchTerm = query.q;
+
+	if (searchTerm) {	
+		res.render("articles/index", {articles: results});
+	} else {
+		articles.forEach(function(article){
+			
+			var isTitleMatch = article.title.indexOf("searchTerm") != -1;
+			var isBodyMatch = article.body.indexOf("searchTerm") != -1;
+			if(isTitleMatch || isBodyMatch) {
+				results.push(article);
+			}
+		});
+		res.render("articles/index", {articles: results});
+	} 
 });
 
 app.get('/articles/new', function(req, res) {
