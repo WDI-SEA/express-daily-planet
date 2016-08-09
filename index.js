db73741d12fa5a8d8ecf6736d780219072815d12
teemopower <jimmykim925@gmail.com>
2016-08-08 22:17:57 -0700

    updated files

---
+++ index.js
@@ -0,0 +1,65 @@
+// requires
+var express = require("express");
+var bodyParser = require("body-parser");
+var fs = require("fs");
+var app = express();
+
+var fileContents = fs.readFileSync('data.json');
+var data = [];
+data = JSON.parse(fileContents);
+//console.log(data);
+
+//set and use statements
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: false}));
+
+// routes
+app.get("/", function(req, res){
+  //res.send("hello");
+  res.render("index", data);
+
+});
+
+app.get("/articles", function(req, res){
+  res.render('articles/index', {myArticles: data});  
+});
+
+// POST 
+app.post('/articles', function(req,res){
+  //console.log(req.body);
+  
+  data.push(req.body);
+
+  fs.writeFileSync('data.json', JSON.stringify(data));
+
+  res.redirect('/articles');
+});
+
+//ABOUT
+app.get("/site/about", function(req, res){
+ 
+  res.render('about');  
+});
+
+// CONTACT
+app.get("/site/contact", function(req, res){
+ 
+  res.render('contact');  
+});
+
+app.get('/articles/:index', function(req, res){
+  res.render('articles/show', {myArticles: data[parseInt(req.params.index)]});
+});
+
+
+
+
+// listen
+app.listen(3000);
+
+
+
+
+
+
+
