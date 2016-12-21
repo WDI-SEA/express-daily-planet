f2e0b1e8e39d0a213cbd2c2d3205008aec42f373
Tommy-Lim <tlim24@gmail.com>
2016-12-20 23:00:02 -0800

    restructure files and added about, contact, and button formats

--- index.js
+++ index.js
@@ -19,36 +19,42 @@ app.use(bodyParser.urlencoded({extended: false})); //boilerplate
 
 //goes to home page of site
 app.get('/',function(req,res){
-  res.render('home');
+  res.render('site/home');
 });
 
 //goes to all articles
-app.get('/articles', function(req,res){
+app.get('/articles/index', function(req,res){
   db.news.findAll().then(function(articles){
-    res.render('articles',{articles:articles});
+    res.render('articles/index',{articles:articles});
   });
 });
 
 //goes to article submission page
 app.get('/articles/new', function(req,res){
-  res.render('articlesNew');
+  res.render('articles/new');
 });
 
 //posts article to articles
 app.post('/articles/new',function(req,res){
   console.log(req.body);
   db.news.create(req.body).then(function(article){
-    res.redirect('/articles');
+    res.redirect('index');
   });
 });
 
-app.get('/article/:id', function(req,res){
+app.get('/articles/show/:id', function(req,res){
   db.news.findById(req.params.id).then(function(article){
-    res.render('article',{article:article});
+    res.render('articles/show',{article:article});
   });
 });
 
+app.get('/about', function(req,res){
+  res.render('site/about');
+});
 
+app.get('/contact', function(req,res){
+  res.render('site/contact');
+});
 
 
 //LISTEN
