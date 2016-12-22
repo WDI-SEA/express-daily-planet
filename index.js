0d222f4cf1b89ad1c901930af2c8f65c24698a0d
brianbixby0@gmail.com <brianbixby0@gmail.com>
2016-12-22 08:47:43 -0800

    not working

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var ejsLayouts = require("express-ejs-layouts");
+var path = require('path');
 //global variables
 var app = express();
 var db = require("./models");
@@ -12,6 +13,7 @@ app.use(ejsLayouts);
 app.use(bodyParser.urlencoded({
     extended: false
 }));
+app.use(express.static(path.join(__dirname, 'static')));
 
 //routes
 //about
@@ -25,7 +27,7 @@ app.get("/contact", function(req, res) {
 // home page
 app.get("/", function(req, res) {
     // res.send('HELLO TACO!!!');
-    res.render("site/home");
+    res.render("site/index.ejs");
 });
 
 // gets all articles  db. is based on folder models name
@@ -62,6 +64,35 @@ app.post("/articles/new", function(req, res) {
     db.article.create(req.body).then(function(article) {
         res.redirect("/articles");
         // console.log(req.body);
+    });
+});
+// delete articles and redirects to articles
+app.delete("/article/:id", function(req, res) {
+    db.article.findById(req.params.id).then(function(article) {
+        article.destroy();
+        console.log(req.params.id);
+        res.send({
+            message: 'success destroying'
+        });
+    });
+});
+
+//updates article with given id
+app.put('/article/:id', function(req, res) {
+    db.article.findById(req.params.id).then(function(article) {
+        article.update(req.body);
+        res.send({
+            message: 'success putting'
+        });
+    });
+});
+
+//goes to edit form for article with id
+app.get('/article/:id/edit', function(req, res) {
+    db.news.findById(req.params.id).then(function(article) {
+        res.render('articles/editArticle', {
+            article: article
+        });
     });
 });
 
