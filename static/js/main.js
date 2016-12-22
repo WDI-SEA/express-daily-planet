0ed26e36eafa5940d30fd1c96509d69fe10e3406
Tommy-Lim <tlim24@gmail.com>
2016-12-21 22:44:43 -0800

    added css static

--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,9 @@
+// - Were we supposed to be filling in text with DOM manipulation or refreshing pages?
+// - Does ordering of routes in index.js matter? e.g., when i used /articles/:id
+// and /articles/new was requested, it didnt recognize the "new" article.
+// - Do we always need to send a response be it render or send data?
+
+
 console.log('js loaded');
 
 $('.delete-link').on('click', function(e) {
