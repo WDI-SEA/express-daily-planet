29c86af8a25670287eef2fb9be23e440891293a9
Tommy-Lim <tlim24@gmail.com>
2016-12-22 08:55:02 -0800

    sanitized text input

--- models/news.js
+++ models/news.js
@@ -2,7 +2,7 @@
 module.exports = function(sequelize, DataTypes) {
   var news = sequelize.define('news', {
     title: DataTypes.STRING,
-    body: DataTypes.STRING
+    body: DataTypes.TEXT
   }, {
     classMethods: {
       associate: function(models) {
