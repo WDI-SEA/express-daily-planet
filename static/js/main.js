3bb8a3b7cc2422e749f571e98dd58a7584e53c33
Tommy-Lim <tlim24@gmail.com>
2016-12-21 23:25:32 -0800

    added comments to main.js and reversed search and articles to get newest at top

--- static/js/main.js
+++ static/js/main.js
@@ -2,10 +2,11 @@
 // - Does ordering of routes in index.js matter? e.g., when i used /articles/:id
 // and /articles/new was requested, it didnt recognize the "new" article.
 // - Do we always need to send a response be it render or send data?
-
+// - Added the .reverse() to order newest at the top!
 
 console.log('js loaded');
 
+//AJAX for deleting a link
 $('.delete-link').on('click', function(e) {
   e.preventDefault();
 
@@ -22,6 +23,7 @@ $('.delete-link').on('click', function(e) {
 
 });
 
+//AJAX for updating a link
 $('#edit-form').on('submit', function(e){
   e.preventDefault();
 
@@ -37,4 +39,9 @@ $('#edit-form').on('submit', function(e){
     console.log(data);
     window.location = '/articles/';
   });
+});
+
+//JS for toggling submission form
+$('#toggle-add').click(function(){
+  $('#add-form').toggle();
 });
