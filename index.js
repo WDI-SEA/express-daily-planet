b194f7e9d452cb7325e52bdb28a80f3a6be3008a
rallysport206 <phan.tony125@gmail.com>
2017-12-20 18:04:42 -0800

    early commit

---
+++ index.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var ejsLayouts = require('express-ejs-layouts');
+var app = express();
+
+app.set('view engine', 'ejs');
+app.use(ejsLayouts);
+app.get('/', function(req, res) {
+  res.render('index');
+});
+app.listen(3000)
