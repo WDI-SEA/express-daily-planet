a5f266c6fe47bdc543315fa9711f530c2f057305
Nick Nedev <nedevn@gmail.com>
2017-12-20 22:02:47 -0800

    Initial Commit

---
+++ index.js
@@ -0,0 +1,20 @@
+//Modules and global vars
+let express = require('express');
+let app = express();
+let ejsLayouts = require('express-ejs-layouts');
+let bodyParser = require('body-parser');
+
+//Set snd Use statements for middleware
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(ejsLayouts);
+
+//Home routels
+app.get('/', (req,res)=>{
+    res.send('yay');
+})
+
+
+
+//Listen on port 3003
+app.listen(3003);
