81eb339c6ae74a10a6cf2aa8b7aa4c4423ac307b
jacquelynmarcella <jacquelyn.marcella@gmail.com>
2017-12-20 20:27:45 -0800

    Got server/node talking to each other and the main files connected through ejs

---
+++ controllers/articles.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var router = express.Router();
+var db = require('../models');
+
+router.get('/', function(req, res){
+	db.article.findAll().then(function(articles){
+		res.render('articles', {articles: articles});	
+	});
+});
+
+router.get('/:id', function(req, res){
+	db.article.findById(req.params.id).then(function(article){
+		res.render('articles/show', { article: article });
+	});
+});
+
+module.exports = router;
