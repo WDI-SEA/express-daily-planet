04953f1a6fd0cc3776475bfc5e838d24bd415258
Nick Nedev <nedevn@gmail.com>
2017-12-20 23:49:35 -0800

    Adds About and Contact Routes

--- controllers/articles.js
+++ controllers/articles.js
@@ -2,12 +2,27 @@ let express = require('express');
 let router = express.Router();
 let db = require('../models');
 
+
+// Render all the articles
 router.get('/', (req, res)=>{
-    res.send('yayayayayaya');
+    db.article.findAll().then((articles)=>{
+         res.render('../views/articles/index.ejs', {articles:articles});
+    })
+   
 });
 
 router.get('/new', (req,res)=>{
-    res.send('This is SPARTA!!');
-})
+    // res.send('This is SPARTA!!');
+    res.render('../views/articles/new.ejs', {});
+});
+
+router.get('/:id', (req, res)=>{
+
+});
+
+// db.article.create({
+//     title: 'Neue Deutsche Härte still angry?',
+//     body: 'Hangry, we meant hangry.'
+// });
 
 module.exports = router;
--- index.js
+++ index.js
@@ -6,6 +6,7 @@ let bodyParser = require('body-parser');
 
 //Set snd Use statements for middleware
 app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(ejsLayouts);
  
@@ -14,9 +15,15 @@ app.use('/articles', require('./controllers/articles.js'));
 //Home route
 app.get('/', (req,res)=>{
     res.render('index',{});
+});
+
+app.get('/about', (req, res)=>{
+    res.send('About');
+});
+
+app.get('/contact', (req,res)=>{
+    res.send('contact');
 })
-
-
 
 //Listen on port 3003
 app.listen(3003);
