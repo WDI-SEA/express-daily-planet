493f114a4365d42958ea0b5d047940efca536dc4
jacquelynmarcella <jacquelyn.marcella@gmail.com>
2017-12-20 22:10:03 -0800

    Added important cat images and styling

--- index.js
+++ index.js
@@ -7,12 +7,23 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(ejsLayouts);
 
+// Link to CSS
+app.use(express.static('public'));
+
 //Link to controller
 app.use('/articles', require('./controllers/articles'));
 
 // Declare routes 
 app.get('/', function(req, res){
-	res.render('index');
+	res.render('site/home');
+});
+
+app.get('/about', function(req, res){
+	res.render('site/about');
+});
+
+app.get('/contact', function(req, res){
+	res.render('site/contact');
 });
 
 app.listen(3000);
