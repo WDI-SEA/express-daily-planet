dd28c8db6cdeda037bc94975a29e6315b3f45bfa
aishabraud92 <aishabraud@gmail.com>
2017-12-22 13:14:41 -0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,59 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var fs = require("fs");
+var ejsLayouts = require("express-ejs-layouts");
+var app = express();
+
+
+ app.use(bodyParser.urlencoded({ extended: false }));
+ app.set("view engine", "ejs");
+ app.use(ejsLayouts);
+ app.get("/", function(req, res) {
+ 	res.render("sites/index");
+ });
+
+ app.get("/articles", function(req, res) {
+ 	var articlesFromJSONFILE = fs.readFileSync("./data.json");
+ 	var parsedArticles = JSON.parse(articlesFromJSONFILE);
+ 	  res.render("articles/index", {articlesFileObject : parsedArticles});
+
+ });
+
+app.post("/articles/new", function(req, res) {
+var articlesFromJSONFILE = fs.readFileSync("./data.json");
+var parsedArticles = JSON.parse(articlesFromJSONFILE);
+ 	parsedArticles.push(req.body);
+
+fs.writeFileSync("./data.json", JSON.stringify(parsedArticles));
+    res.redirect("/articles");
+
+ });
+
+app.get("/articles/new", function(req, res) {
+ 	res.render("articles/new");
+
+ });
+
+ app.get("/articles/show/:x", function(req,res) {
+   var articlesFromJSONFILE = fs.readFileSync("./data.json");
+   var parsedArticles = JSON.parse(articlesFromJSONFILE);
+   var num = req.params.x
+
+if(!isNaN(num) && num >= 0 && num < parsedArticles.length) {
+ 		res.send(parsedArticles[num]);
+
+} else {
+ 		res.send("This is not a valid article number.");
+
+ 	  }
+ });
+
+ app.get("/sites/contact", function(req, res) {
+ 	res.render("sites/contact");
+
+  });
+
+ app.get("/sites/about", function(req, res) {
+ 	res.render("sites/about");
+
+  });
