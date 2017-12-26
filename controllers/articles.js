02a7efa5c53e06ca4e9604289ba37b676c71bc07
sweeneyyy <sweeneyarnett@gmail.com>
2017-12-26 12:58:19 -0800

    finished deliverable no bonus yet

---
+++ controllers/articles.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var router = express.Router();
+var db = require('../models');
+
+router.get('/', function(req, res){
+  db.article.findAll().then(function(articles){
+    res.render('articles/index', {results: articles});
+  });
+});
+
+router.post('/', function(req, res){
+  db.article.create(req.body).then(function(createdArticle){
+    res.redirect('/articles');
+  }).catch(function(err){
+    res.send('Uh oh', err);
+  });
+});
+
+router.get('/new', function(req, res){
+    res.render('articles/new');
+  });
+
+router.get('/:id', function(req, res){
+  db.article.findById(req.params.id).then(function(article){
+    res.render('articles/show', { result: article });
+  });
+});
+
+
+
+module.exports = router;
