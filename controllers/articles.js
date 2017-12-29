94ade5a0bee57febdcf76685485efdd03bbdbd47
Joe Johnson <joe@Joes-MacBook-Pro.local>
2017-12-29 11:10:52 -0800

    Shows buttons and article database.

--- controllers/articles.js
+++ controllers/articles.js
@@ -11,31 +11,31 @@ router.get('/', function(req, res){
   // res.render('articles/all');  <- moved into finaAll function
 });
 
-// router.post('/', function(req, res){
-//   db.article.create(req.body).then(function(createdArticle){
-//     res.redirect('/articles/' + createdArticle.id);
-//   }).catch(function(err){
-//     res.send('uh oh!', err);
-//   });
-// });
+router.post('/', function(req, res){
+  db.article.create(req.body).then(function(createdArticle){
+    res.redirect('/articles/' + createdArticle.id);
+  }).catch(function(err){
+    res.send('uh oh!', err);
+  });
+});
 
-// router.delete('/:id', function(req, res){
-//   console.log('Delete route. Id = ', req.params.id);
-//   // res.send('Delete Route Workin');
-//   db.article.destroy({
-//     where: { id: req.params.id }
-//   }).then(function(deleted){
-//     console.log('delete = ', deleted);
-//     res.send('successful');
-//   }).catch(function(err){
-//     console.log('Error occured', err);
-//     res.send('fail');
-//   })
-// });
+router.delete('/:id', function(req, res){
+  console.log('Delete route. Id = ', req.params.id);
+  // res.send('Delete Route Workin');
+  db.article.destroy({
+    where: { id: req.params.id }
+  }).then(function(deleted){
+    console.log('delete = ', deleted);
+    res.send('successful');
+  }).catch(function(err){
+    console.log('Error occured', err);
+    res.send('fail');
+  })
+});
 
-// router.get('/new', function(req, res){
-//   res.render('articles/new');
-// });
+router.get('/new', function(req, res){
+  res.render('articles/new');
+});
 
 
 router.get('/:id', function(req, res){
