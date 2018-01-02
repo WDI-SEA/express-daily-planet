d69581dc876158516d05934ec71d93cbf236f59e
Winston Summers <winstonsummers@WinstonmatoMacBook-Pro.local>
2018-01-02 10:54:10 -0800

    reached mvp

--- index.js
+++ index.js
@@ -5,18 +5,42 @@ var fs = require('fs');
 var app = express();
 
 app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({extended: false}));
 app.use(ejsLayouts);
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(__dirname + '/public'));
 
+var articles = fs.readFileSync('./data.json');
+articles = JSON.parse(articles);
+
 app.get('/', function(req, res) {
-  res.render('index.ejs');
+  res.render('site/index.ejs');
+});
+
+app.get('/contact', function(req, res) {
+  res.render('site/contact.ejs');
+});
+
+app.get('/about', function(req, res) {
+  res.render('site/about.ejs');
 });
 
 app.get('/articles', function(req, res) {
-  var articles = fs.readFileSync('./data.json');
-  articles = JSON.parse(articles);
   res.render('articles/index.ejs', {articles: articles});
+});
+
+app.get('/articles/new', function(req, res) {
+  res.render('articles/new.ejs')
+});
+
+app.post('/articles', function(req, res) {
+  articles.push(req.body);
+  fs.writeFileSync('./data.json', JSON.stringify(articles));
+  res.redirect('/articles');
+});
+
+app.get('/articles/:idx', function(req, res) {
+  var articleIndex = parseInt(req.params.idx);
+  res.render('articles/show', {articles: articles[articleIndex]});
 });
 
 app.listen(3000);
