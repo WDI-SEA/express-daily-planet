d43da5edbf8f54445c4b15d09762528c43ff92ef
Brett <brettspencr@gmail.com>
2018-02-13 09:27:53 -0800

    Fixed CSS loading issue with the class' help

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 var express = require("express");
 var fs = require("fs");
+var path = require("path");
 var bodyParser = require("body-parser");
 
 var app = express();
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: false}));
  
