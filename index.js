c77294894811faad35f79e81d94e189dd59f856d
Emily Garrett <emilygarrett@Emilys-MacBook-Pro.local>
2018-02-12 22:51:56 -0800

    redid everything oops but very little styling

--- index.js
+++ index.js
@@ -1,51 +1,55 @@
 var express = require('express');
-var app = express();
 var fs = require('fs');
 var bodyParser = require('body-parser');
+var path = require('path');
 
+var app = express();
 app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.urlencoded({extended: false}));
 
-//get for home
+//gets site/index
 app.get('/', function(req, res) {
-	res.render('index.ejs');
+  res.render('site/index');
 });
 
-//get for /articles
-app.get('/articles', function(req, res) {
-	var fileContents = fs.readFileSync('./data.json');
-	fileContents = JSON.parse(fileContents);
-	res.render('articles/index', {articles: fileContents});
+//gets site/about
+app.get('/about', function(req, res) {
+  res.render('site/about');
 });
 
-
-//get for /site/about STATIC
-app.get('/site/about', function(req, res) {
-	res.render('site/about');
+//gets site/contact info
+app.get('/contact', function(req, res) {
+  res.render('site/contact');
 });
 
-//get for /site/contact STATIC
-app.get('/site/contact', function(req, res) {
-	res.render('site/contact');
+//gets the /articles
+app.get("/articles", function(req, res) {
+  var fileContents = fs.readFileSync('./data.json');
+  fileContents = JSON.parse(fileContents);
+  res.render('articles/index', {articles: fileContents});
 });
 
-//get for articles/new
+//gets the /articles/new page where you can build a new article
 app.get('/articles/new', function(req, res) {
-	res.render('articles/new');
+  res.render("articles/new");
 });
 
-//displays new article info
-app.post('/articles/new', function(req, res) {
-	res.send(req.body);
+//gets a specific article from url params
+app.get("/articles/:idx", function(req, res) {
+  var articles = fs.readFileSync("./data.json");
+  articles = JSON.parse(articles);
+  // get the array index from the url params
+  var articleIndex = parseInt(req.params.idx);
+  res.render('articles/show', {article: articles[articleIndex]});
 });
 
-//get for /articles/id
-app.get('/articles/:idx', function(req, res) {
-	var fileContents = fs.readFileSync('./data.json');
-	fileContents = JSON.parse(fileContents);
-	var articleIdx = parseInt(req.params.idx);
-	res.render('articles/show', { thisArticle: fileContents[articleIdx]})
+//posts to articles from the new page
+app.post("/articles", function(req, res){
+  var articles = fs.readFileSync("./data.json");
+  articles = JSON.parse(articles);
+  articles.push(req.body);
+  fs.writeFileSync('./data.json', JSON.stringify(articles));
+  res.send(req.body);
 });
-
 
 app.listen(3000);
