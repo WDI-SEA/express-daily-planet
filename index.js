8ed658dd548f1c9e9c37f97d49012997db582071
Sean Cesmat <sean.cesmat@gmail.com>
2018-02-13 13:44:12 -0800

    got external css file working | added PUT and DELETE

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ var app = express();
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static('public'));
 
 
 app.get('/', function(req, res) {
@@ -39,11 +40,18 @@ app.post('/articles', function(req, res) {
   res.redirect('/articles');
 });
 
-app.get('/articles/:id', function(req, res) {
+app.get('/articles/:idx', function(req, res) {
   var fileContents = fs.readFileSync('./data.json');
   var data = JSON.parse(fileContents);
-  var dataIndex = parseInt(req.params.id);
+  var dataIndex = parseInt(req.params.idx);
   res.render('articles/show', {articles: data[dataIndex]});
+});
+
+app.get('/articles/:idx/edit', function(req, res) {
+  var fileContents = fs.readFileSync('./data.json');
+  var data = JSON.parse(fileContents);
+  var dataIndex = parseInt(req.params.idx);
+  res.render('articles/edit', {article: data[dataIndex], id: dataIndex});
 });
 
 app.get('/about', function(req, res) {
@@ -54,6 +62,35 @@ app.get('/about', function(req, res) {
 app.get('/contact', function(req, res) {
   console.log('You are in the /contact');
   res.render('site/contact');
+});
+
+app.delete('/articles/:idx/destroy', function(req, res) {
+  console.log('removing article route');
+  //read from the file
+  var fileContents = fs.readFileSync('./data.json');
+  //parse the contents
+  var data = JSON.parse(fileContents);
+  var dataIndex = parseInt(req.params.idx);
+  //splice the selected element out of the file contents
+  data.splice(dataIndex, 1);
+  // re-stringify the array and write it back to the file
+  fs.writeFileSync('./data.json', JSON.stringify(data));
+  res.redirect('/articles')
+});
+
+app.put('/articles/:idx', function(req, res) {
+  console.log('put /articles/idx');
+  var articleToEdit = req.params.idx;
+  // read the files
+  var fileContents = fs.readFileSync('./data.json');
+  //parse the contents
+  var data = JSON.parse(fileContents);
+  // find the item at the selected index and update it
+  // var dataIndex = parseInt(req.params.idx);
+  data.splice(articleToEdit, 1, req.body);
+  //re-stringify the object and write to the file
+  fs.writeFileSync('./data.json', JSON.stringify(data));
+  res.send('Yeah dawg!');
 });
 
 app.listen(8000, function() {
