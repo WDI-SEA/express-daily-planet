ec3956f9afc5e305710defdcbb17cbaa51c501b5
Lars Nelson <lnelson7820@gmail.com>
2020-04-13 21:07:10 -0700

    Something weird is going on with the CSS

--- index.js
+++ index.js
@@ -41,20 +41,21 @@ app.get('/articles', (req,res) => {
 app.get('/articles/new', (req,res) => {
     res.render('articles/new')
 })
-// Save a newly made article to the database
-app.post('/articles', (req,res) => {
-    db.articles.create(req.body)
-    .then(newArticle => {res.redirect('/articles')})
-    .catch(err => {res.send("Yeah, something went wrong")})
-})
 // Retrieve an article of specified ID
-app.get('/articles/:idx', (req,res) => {
+app.get('/articles/show/:idx', (req,res) => {
+    console.log("Is the show index page being called?")
     db.articles.findAll({where: {id:req.params.idx}})
     .then(article => {
         article = article[0]
         res.render('articles/show', {article})
     })
     .catch(err => {console.log("Error: ", err)})
+})
+// Save a newly made article to the database
+app.post('/articles', (req,res) => {
+    db.articles.create(req.body)
+    .then(newArticle => {res.redirect('/articles')})
+    .catch(err => {res.send("Yeah, something went wrong")})
 })
 // Display the "About" HTML page
 app.get('/about', (req,res) => {
