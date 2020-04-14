a3f33d20dfd6d036e3e94f65498ce1bb85fc8023
davidvelichko52 <davidvelichko52@gmail.com>
2020-04-13 22:47:14 -0700

    got routes

--- index.js
+++ index.js
@@ -1,1 +1,56 @@
 // Your code here
+let express = require('express')
+let layouts = require('express-ejs-layouts')
+
+let app = express()
+
+app.set('view engine', 'ejs')
+
+app.use(express.static('static'))
+
+app.use(layouts)
+
+///////////////////////////////////////////////////////////
+app.get('/', function(req, res) {
+    res.render('index.ejs');
+  });
+app.get('/articles', function(req, res) {
+    res.render('articles/index.ejs');
+  });
+app.get('/articles/new', function(req, res) {
+    res.render('articles/new.ejs');
+  });
+  
+  app.post('/faves', (req, res) => {
+    db.Article.findAll()
+    .then(farticles => {
+  // TODO: Pass articles array to relevant EJS page
+})
+    .catch(err => {
+        console.log('ERROR:', err)
+        res.send('Uh oh!')
+    })
+})
+  
+app.get('/articles/:id', function(req, res) {
+    res.render('articles/show.ejs');
+});
+
+  ////////////////////////////////////////////////////////////////
+
+
+// app.get('/about', (req, res) = {
+    
+// })
+// app.get('/contact', (req, res) = {
+
+// })
+
+
+
+
+
+// pick a port
+app.listen(3000, () => {
+    console.log('ready to 💪')
+})
